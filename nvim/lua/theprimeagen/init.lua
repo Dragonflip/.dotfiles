4fc4dc1066f9104d7a53154c5759726ba3383124
Joao Paulo <jpmarques@ufrn.edu.br>
2022-12-18 11:55:14 +0000

    adicionando cmp-lsp e tmux

--- nvim/lua/theprimeagen/init.lua
+++ nvim/lua/theprimeagen/init.lua
@@ -1,2 +1,4 @@
 require("theprimeagen.set")
 require("theprimeagen.remap")
+require("theprimeagen.lspconfig")
+require("theprimeagen.cmp-config")
--- nvim/lua/theprimeagen/package.lua
+++ nvim/lua/theprimeagen/package.lua
@@ -3,5 +3,17 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function()
     --Packer can manage itself
     use 'wbthomason/packer.nvim'
+
+    --Colorscheme
     use 'folke/tokyonight.nvim'
+
+    --LSP config
+    use 'neovim/nvim-lspconfig'
+
+    --Autocomplete
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-cmdline'
+    use 'hrsh7th/nvim-cmp'
 end)
